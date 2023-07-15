7605c76385a25f0ad6d2879b9a8d4c3042e8ce4f
Gabriel <139657979+klowngabe@users.noreply.github.com>
2023-07-15 16:21:22 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+// Declare your genre arrays here
+let comedy = ["https://m.media-amazon.com/images/M/MV5BNGQwZjg5YmYtY2VkNC00NzliLTljYTctNzI5NmU3MjE2ODQzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg","https://upload.wikimedia.org/wikipedia/en/d/d4/Ted_%28film%29.png", "https://m.media-amazon.com/images/M/MV5BYTAzMmIwMWQtYjRjYy00NmM2LWIxYWUtY2NhYjMzZjc4ZGZmXkEyXkFqcGdeQXVyNDc3MzQ5Nzc@._V1_.jpg"]
+  , action = ["https://m.media-amazon.com/images/M/MV5BZDA4YmE0OTYtMmRmNS00Mzk2LTlhM2MtNjk4NzBjZGE1MmIyXkEyXkFqcGdeQXVyMTMzNDExODE5._V1_FMjpg_UX1000_.jpg", "https://m.media-amazon.com/images/I/51ClgbAfjgL.jpg", "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C1C0181ED7342217394E5AC940003A8653E3311B6016194749B29A0FE3F238EB/scale?width=1200&aspectRatio=1.78&format=jpeg"]
+  , horror = ["https://www.intransitbroadway.com/wp-content/uploads/2022/04/The-Conjuring-Movies-1024x489.png", "https://m.media-amazon.com/images/M/MV5BMTU4NzA4MDEwNF5BMl5BanBnXkFtZTgwMTQxODYzNjM@._V1_FMjpg_UX1000_.jpg","https://froontrowmovies.files.wordpress.com/2015/08/the-dark-promise-of-sinister-2-in-one-single-image-347629.jpg"];
+
+// Make sure to declare your HTML elements as variables! 
+
+// Submit Button 
+
+let submitButton = document.querySelector("button"); 
+submitButton.onclick = function() {
+}; 
+
+
